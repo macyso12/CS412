e1ba6ad64cc1521e81dfc70af626e0c19a4448d0
Macy So <macyso12@gmail.com>
2023-11-13 14:23:19 -0500

    13/11 Updated with redis

--- PS4-weather-app/index.js
+++ PS4-weather-app/index.js
@@ -6,6 +6,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 const axios = require('axios');
 const request = require('request-promise');
 const path = require('path');
+//const fetch = require('node-fetch');
+const redis = require('redis');
 
 //new 
 app.use(bodyParser.json());
@@ -14,8 +16,9 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/page.html")
 })
 
-// POST method
-app.post("/",function(req,res){
+// POST method ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*app.post("/",function(req,res){
     const cityName = req.body.cityName
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName +"&appid=49714856607f1addeeb0d101ff660dfe&units=metric"
     https.get(url, function(response){
@@ -32,55 +35,56 @@ app.post("/",function(req,res){
             res.send();
         })
     })
-})
+})*/
+
+const redisClient = redis.createClient();
 
 app.post('/promise', (req, res) => {
-    // Extract parameters from the request body
     const city = req.body.cityName;
   
-    // Check if required parameters are provided
-    if (!city ) {
-      return res.status(400).json({ error: 'City are required' });
+    if (!city) {
+      return res.status(400).json({ error: 'City is required' });
     }
   
-    // Define the external API URL (replace with the actual API endpoint)
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=49714856607f1addeeb0d101ff660dfe&units=metric`;
+    // Check if the data is in the cache
+    redisClient.get(city, (err, cachedData) => {
+      if (err) {
+        console.error('Error retrieving data from cache:', err);
+        return res.status(500).json({ error: 'Error retrieving data from cache' });
+      }
   
-    // Wrap the request in a Promise
-    const makeRequest = new Promise((resolve, reject) => {
-      request.post(apiUrl, (error, response, body) => {
+      if (cachedData) {
+        // If data is in the cache, return it
+        const weatherData = JSON.parse(cachedData);
+        return res.json({ source: 'cache', weatherData });
+      }
+  
+      // If data is not in the cache, make the API request
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=49714856607f1addeeb0d101ff660dfe&units=metric`;
+  
+      request.get(apiUrl, (error, response, body) => {
         if (error) {
-          reject(error);
-        } else {
-          resolve(body);
+          console.error('Error making API request:', error);
+          return res.status(500).json({ error: 'Error making API request' });
         }
+  
+        const weatherData = JSON.parse(body);
+  
+        // Cache the data with a 15-second timeout
+        redisClient.setex(city, 15, JSON.stringify(weatherData));
+  
+        // Send the response indicating it's not from cache
+        res.json({ source: 'api', weatherData });
+      });
+  
+      redisClient.on('error', (err) => {
+        console.error('Redis error:', err);
       });
     });
-  
-    // Handle the Promise
-    makeRequest
-      .then(data => {
-      // Parse and send the data
-      const weatherData = JSON.parse(data);
-      //res.json(weatherData);
-      const temp = weatherData.main.temp
-      const des = weatherData.weather[0].description
-      const icon = weatherData.weather[0].icon
-      const imageurl = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
-      res.write("<h1> The temp in " + city + " is " + temp + " degree celcius</h1>");
-      res.write("<p> The weather description is " + des + "</p>");
-      res.write("<img src=" +imageurl + ">");
-      res.write("<button onclick=history.back()> Go Back </button>")
-      res.send()
-      })
-      .catch(error => {
-        // Handle errors
-        console.error('Error retrieving weather data:', error);
-        res.status(500).json({ error: 'Error retrieving weather data' });
-      });
   });
 
-//async & awaits
+//async & awaits ----------------------------------------------------------------------------------------------------------------
+
 app.post('/getWeather', async (req, res) => {
     // Extract parameters from the request body
     const city = req.body.cityName;
@@ -122,7 +126,7 @@ app.post('/getWeather', async (req, res) => {
     }
   });
 
-//CallBack Method
+//CallBack Method --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 app.post('/callBack', (req, res) => {
     // Extract parameters from the request body
     const city = req.body.cityName;
